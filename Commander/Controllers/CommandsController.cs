d4b1a5d7bc102596e334c58cee4195dd224c13ce
perfect igbadumhe <perfect.igbadumhe@gmail.com>
2021-01-13 12:40:37 +0100

    first commit

---
+++ Commander/Controllers/CommandsController.cs
@@ -0,0 +1,86 @@
+﻿using AutoMapper;
+using Commander.Data;
+using Commander.Dtos;
+using Commander.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Commander.Controllers
+{
+    //api commands
+    [Route("api/commands")]
+    [ApiController]
+    public class CommandsController : ControllerBase
+    {
+        // private readonly MockCommanderRepo _repo = new MockCommanderRepo();
+        private readonly ICommanderRepo _repo;
+        private readonly IMapper _mapper;
+
+        public CommandsController(ICommanderRepo repo, IMapper mapper)
+        {
+            _repo = repo;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public ActionResult <IEnumerable<CommandReadDto>> GetAllCommands()
+        {
+            var commandItems = _repo.GetAllCommands();
+            return Ok(_mapper.Map<IEnumerable<CommandReadDto>>(commandItems));
+        }
+
+        [HttpGet("{id}", Name ="GetCommandById")]
+        public ActionResult<CommandReadDto> GetCommandById(int id)
+        {
+            var commandItem = _repo.GetCommandById(id);
+            if(commandItem != null)
+            {
+                return Ok(_mapper.Map<CommandReadDto>(commandItem));
+            }
+            return NotFound();
+        }
+
+        [HttpPost]
+        public ActionResult<CommandReadDto> CreateCommand(CommandCreateDto commandCreateDto)
+        {
+            var commandModel = _mapper.Map<Command>(commandCreateDto);
+            _repo.CreateCommand(commandModel);
+            _repo.SaveChanges();
+            var commandDto = _mapper.Map<CommandReadDto>(commandModel);
+            return CreatedAtRoute("GetCommandById", new { id = commandDto.Id }, commandDto );
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult UpdateCommand(int id, CommandUpdateDto commandUpdateDto)
+        {
+            var commandModelFromRepo = _repo.GetCommandById(id);
+            if(commandModelFromRepo== null)
+            {
+                return NotFound();
+            }
+
+            _mapper.Map(commandUpdateDto, commandModelFromRepo);
+
+            _repo.UpdateCommand(commandModelFromRepo);
+            _repo.SaveChanges();
+
+            return NoContent();
+        }
+        [HttpDelete("{id}")]
+        public ActionResult DeleteCommand(int id)
+        {
+            var commandModelFromRepo = _repo.GetCommandById(id);
+            if(commandModelFromRepo == null)
+            {
+                return NoContent();
+            }
+            _repo.DeleteCommand(commandModelFromRepo);
+            _repo.SaveChanges();
+
+            return NoContent();
+        }
+    }
+}
---
+++ Commander/Data/MockCommanderRepo.cs
@@ -0,0 +1,48 @@
+﻿using Commander.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Commander.Data
+{
+    public class MockCommanderRepo : ICommanderRepo
+    {
+        public void CreateCommand(Command cmd)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteCommand(Command cmd)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Command> GetAllCommands()
+        {
+            var commands = new List<Command>
+            {
+                new Command{Id=2, HowTo="Cook rice", Line="wash tge rice", Platform="My house"},
+                new Command{Id=3, HowTo="play music", Line="on music", Platform="sportify"},
+                new Command{Id=4, HowTo="play video", Line="Watch movie", Platform="Laptop"}
+            };
+
+            return commands;
+        }
+
+        public Command GetCommandById(int id)
+        {
+            return new Command { Id = 1, HowTo = "Boil egg", Line = "Boil water", Platform = "COOKING POT" };
+        }
+
+        public bool SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateCommand(Command cmd)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
